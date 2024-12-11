03b2776630b464ec96938ee14f19bd75335f3f0c
lequan <lequan2002.ql@gmail.com>
2024-12-12 00:05:02 +0700

    feat: init express server

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import healthRoute from './routes/healthRoute';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/', healthRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
