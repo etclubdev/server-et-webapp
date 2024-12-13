a833912d1990cb916a81768794c54df242b3cb11
lequan <lequan2002.ql@gmail.com>
2024-12-13 12:05:35 +0700

    feat: update health route path

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import healthRoute from './routes/healthRoute';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/', healthRoute);
+app.use('/health', healthRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
