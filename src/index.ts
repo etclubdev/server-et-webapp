77f65e4651558289ba4b1e5b10b91b4980d21f70
DanhNguyen <nguyendanh0922@gmail.com>
2024-12-23 16:26:19 +0700

    feat(US159): change the name of API endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use('/health', healthRoute);
-app.use(etNewsRoute);
+app.use('/etnews-managed/et-news',etNewsRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
