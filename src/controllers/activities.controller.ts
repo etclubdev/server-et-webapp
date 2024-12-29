1dc600260d467b5339d5b12adc960eb20115d73a
avytran <tr.anhvy.work@gmail.com>
2024-12-30 00:51:49 +0700

    feat(US165): add activities controller file

---
+++ src/controllers/activities.controller.ts
@@ -0,0 +1,30 @@
+import activitiesService from "../services/activities.service";
+import { Request, Response } from 'express';
+
+interface activitiesRequest extends Request {
+    id: string,
+    updateData: object
+  }
+  
+const activitiesController = {
+    updateActivity: async ( req: activitiesRequest, res: Response ) => {
+        const id = req.id;
+        const updateData = req.updateData;
+
+        try {
+            const updateActivity = await activitiesService.updateActivity({ id, updateData })
+            res.status(201).json({
+                message: 'The activity updated successfully',
+                data: updateActivity
+            })
+
+        } catch (error) {
+            res.status(500).json({ 
+                message: 'An error occurred while updating the activity.', 
+                error: error.message 
+            });
+        }
+    }
+}
+
+export default activitiesController;
