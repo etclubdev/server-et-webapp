422c1910d97e3a688321572aba8ae2a385a3bff0
avytran <tr.anhvy.work@gmail.com>
2024-12-30 00:53:20 +0700

    feat(US165): update index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 import healthRoute from './routes/health.route';
+import activitiesRoute from './routes/activities.route';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.use('/health', healthRoute);
+app.use('/activities', activitiesRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
