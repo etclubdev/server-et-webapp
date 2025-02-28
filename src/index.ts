6e7a39cab64e1a201320f94d134118780d4a525a
TrgDuyNg <truongduy5425@gmail.com>
2025-02-28 23:37:00 +0700

    feature(P2BI-193): build create api for activity post v2

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
-
+import activityRoutes from "./routes/activity.routes"
 import healthRoute from './routes/health.route';
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 
-app.use('/health', healthRoute);
+app.use(express.json());
+app.use("/activities", activityRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
