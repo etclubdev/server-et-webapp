a7e4761d571ac99a75f3e7d9ede33eaf7eb0731b
TrgDuyNg <truongduy5425@gmail.com>
2025-03-05 23:11:10 +0700

    feat(P2BI-193): rename import from 'activityRoutes' to 'activityRoute'

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import healthRoute from './routes/health.route';
-import activityRoutes from "./routes/activity.routes";
+import activityRoute from "./routes/activity.routes";
 import etBlogRoute from './routes/etBlog.route';
 
 const app = express();
 
 app.use(express.json());
-app.use("/activities", activityRoutes);
+app.use("/activities", activityRoute);
 app.use('/health', healthRoute);
 app.use('/et-blog', etBlogRoute);
 
