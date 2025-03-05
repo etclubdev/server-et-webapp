6f16059d8f666e6ca3250dd2862fa1206e66f8a6
Gia Huy <huyhaibabon861@gmail.com>
2025-03-05 16:26:19 +0700

    fix(P2BI-202): fix route by using /:id instead of /:etnewsid

--- src/controllers/etnews.controller.ts
+++ src/controllers/etnews.controller.ts
@@ -3,11 +3,11 @@ import { Request, Response } from "express";
 import etnewsService from "../services/etnews.service";
 export default {
   updateETNewsController: async (req: Request, res: Response) => {
-    const { etnewsid } = req.params;
+    const { id } = req.params;
     const news = req.body;
     
     try {
-        const updatedNews = await etnewsService.updateETNews(etnewsid, news);
+        const updatedNews = await etnewsService.updateETNews(id, news);
 
         if (!updatedNews) {
             res.status(404).json({ message: "News not found" });
@@ -20,6 +20,7 @@ export default {
       });
       return;
     } catch (error) {
+      console.log(error);
       res.status(500).json({ message: "Internal Server Error " + error.message });
       return;
     }
--- src/routes/etnews.route.ts
+++ src/routes/etnews.route.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
 import etnewsController from '../controllers/etnews.controller';
-import validate from '../middlewares/validateETNews.mdw'; 
+import validate from '../middlewares/validate.mdw'; 
 import { updateNewsSchema }  from '../entities/etnews.entity'
 
 const router = express.Router();
 
-router.put('/:etnewsid', validate(updateNewsSchema),etnewsController.updateETNewsController);
+router.put('/:id', validate(updateNewsSchema),etnewsController.updateETNewsController);
 
 export default router;
