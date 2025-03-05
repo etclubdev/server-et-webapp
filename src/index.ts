5ae4c042a69f55af4ea8d0156e1cec7071985cd6
TrgDuyNg <truongduy5425@gmail.com>
2025-03-05 16:59:54 +0700

    feat(P2BI-194): Build API to Edit a New Activity Post fixed

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,5 @@ app.use('/health', healthRoute);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
