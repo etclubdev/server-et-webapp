1996a24da59d4de560f72bed52a197c21077ddef
Gia Huy <huyhaibabon861@gmail.com>
2025-03-07 21:52:58 +0700

    fix(P2BI-200): rename files

--- src/controllers/etNews.controller.ts
+++ src/controllers/etNews.controller.ts
@@ -1,19 +1,14 @@
 import { Request, Response } from "express";
 
-import etnewsService from "../services/etnews.service";
+import etNewsService from "../services/etNews.service";
 export default {
   getETNewsbyIDController: async (req: Request, res: Response): Promise<void> => {
     try {
       const { id } = req.params;
-      const news = await etnewsService.getETNewsbyID(id);
+      const news = await etNewsService.getETNewsbyID(id);
 
       if (!news) {
         res.status(404).json({ message: "News not found" });
-        return;
-      }
-
-      if (!news.visible) {
-        res.status(416).json({ message: " Requested range not satisfiable" });
         return;
       }
 
--- src/routes/etnews.route.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import etnewsController from '../controllers/etnews.controller';
-
-const router = express.Router();
-
-router.get('/:id', etnewsController.getETNewsbyIDController);
-
-export default router;
