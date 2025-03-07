370215cd736c87b971febb9a1fbf0f54b90029ef
Gia Huy <huyhaibabon861@gmail.com>
2025-03-07 21:46:38 +0700

    fix(P2BI-199): rename files

--- src/routes/etnews.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-import etnewsController from '../controllers/etnews.controller';
-
-const router = express.Router();
-router.get('/', etnewsController.getAllETNews);
-
-export default router;
