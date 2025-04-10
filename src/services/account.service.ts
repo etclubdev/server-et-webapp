81c70363d40de0767dc5af699d9c71bf557acbc2
Gia Huy <huyhaibabon861@gmail.com>
2025-04-10 18:29:50 +0700

    fix(P2BI-472): added necessary fields to account fetch by ID

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -3,7 +3,8 @@ import db from '../utils/db.util';
 export default {
     getAccountById: async(id: string) => {
         const account = await db('account')
-        .select('account_id', 'username', 'sysrole_id', 'created_on', 'last_modified_on')
+        .join('personnel', 'account.personnel_id', 'personnel.personnel_id')
+        .select('account_id', 'account.personnel_id', 'personnel.personnel_name', 'personnel.student_id', 'username', 'sysrole_id')
         .where('account_id', id)
 
         if(account.length === 0) {
