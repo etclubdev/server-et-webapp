7cae2e3dd3a8ea5cb0d2b4ccb311589e3e336d91
giahuytr <huyhaibabon861@gmail.com>
2025-04-12 01:56:28 +0700

    Feature/P2BI-548 Build API To Delete Banner (#70)

--- src/controllers/banner.controller.ts
+++ src/controllers/banner.controller.ts
@@ -76,4 +76,37 @@ export default {
             return;
         }
     },
+    deleteBanner: async (req: Request, res: Response) => {
+        try {
+            const { id } = req.params;
+            const deletedBanner = await bannerService.deleteBanner(id);
+            if (!deletedBanner) {
+                res.status(404).json({ message: 'Not found' });
+                return;
+            }
+            res.status(204).json();
+            return;
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Internal server error' + error.message });
+            return;
+        }
+    },
+    deleteBanners: async (req: Request, res: Response) => {
+        try {
+            const { banners } = req.body;
+            const deletedBanners = await bannerService.deleteBanners(banners);
+
+            if (!deletedBanners) {
+                res.status(404).json({ message: 'Not found' });
+                return;
+            }
+            res.status(204).json();
+            return;
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Internal server error' + error.message });
+            return;
+        }
+    },
 }
