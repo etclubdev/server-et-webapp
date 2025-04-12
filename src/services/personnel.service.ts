bc54c02450be0898c8e22141c2998f1b37805d52
TrgDuyNg <truongduy5425@gmail.com>
2025-04-12 21:07:32 +0700

    fix(P2BI-376): join personnel and personnel_status

--- src/services/personnel.service.ts
+++ src/services/personnel.service.ts
@@ -3,13 +3,32 @@ import { Personnel } from '../types/personnel';
 
 export default {
     getAllPersonnel: async () => {
-        const personnel = await db('personnel')
-            .select('*');
+        const personnels = await db('personnel')
+            .join('personnel_status', 'personnel.personnel_id', 'personnel_status.personnel_id')
+            .select(
+                'personnel.personnel_id',
+                'personnel.student_id',
+                'personnel.personnel_name',
+                'personnel.email',
+                'personnel.dob',
+                'personnel.gender',
+                'personnel.address',
+                'personnel.faculty',
+                'personnel.university',
+                'personnel.major',
+                'personnel.class',
+                'personnel.cv_type',
+                'personnel.cv_link',
+                'personnel.cohort_name',
+                'personnel_status.term_id',
+                'personnel_status.department_name',
+                'personnel_status.position_name',
+                'personnel_status.personnel_status'
+            );
 
-        if (personnel.length === 0) {
+        if (personnels.length === 0) {
             return null;
         }
-        return personnel;
+        return personnels;
     },
-
 };
