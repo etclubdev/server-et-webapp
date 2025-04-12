e48066c85d11f1b7457d1992959e89975880297b
TrgDuyNg <truongduy5425@gmail.com>
2025-04-12 21:15:34 +0700

    fix(P2BI-33386): join database to include all fields of  personnel result

--- src/services/personnel.service.ts
+++ src/services/personnel.service.ts
@@ -2,12 +2,30 @@ import db from '../utils/db.util';
 import { Personnel } from '../types/personnel';
 
 export default {
-
-
-    getPersonnelByID: async (id: string): Promise<Personnel | null> => {
+    getPersonnelByID: async (id: string): Promise<any | null> => {
         const personnel = await db('personnel')
-            .select('*')
-            .where('personnel_id', id)
+            .join('personnel_status', 'personnel.personnel_id', 'personnel_status.personnel_id')
+            .select(
+                'personnel.personnel_id',
+                'personnel.student_id',
+                'personnel.personnel_name',
+                'personnel.email',
+                'personnel.dob',
+                'personnel.gender',
+                'personnel.address',
+                'personnel.faculty',
+                'personnel.university',
+                'personnel.major',
+                'personnel.class',
+                'personnel.cv_type',
+                'personnel.cv_link',
+                'personnel.cohort_name',
+                'personnel_status.term_id',
+                'personnel_status.department_name',
+                'personnel_status.position_name',
+                'personnel_status.personnel_status'
+            )
+            .where('personnel.personnel_id', id)
             .first();
 
         if (!personnel) {
