17f8d8a25a74d02e1badfeea5604b330039ce1d7
Nguyễn Trường Duy <168575514+TrgDuyNg@users.noreply.github.com>
2025-04-14 01:54:59 +0700

    Feature/P2BI-386 Build API To Get Personnel Details By ID (#73)
    * feature(P2BI-386): build api to get personnel details by id
    
    * fix(P2BI-33386): join database to include all fields of  personnel result
    
    * feat(P2BI-386): update package-lock.json
    
    ---------
    
    Co-authored-by: avytran <tr.anhvy.work@gmail.com>

--- src/services/personnel.service.ts
+++ src/services/personnel.service.ts
@@ -2,6 +2,37 @@ import db from "../utils/db.util";
 import { Personnel } from "../types/personnel";
 
 export default {
+    getPersonnelByID: async (id: string): Promise<any | null> => {
+        const personnel = await db('personnel')
+            .join('personnel_status', 'personnel.personnel_id', 'personnel_status.personnel_id')
+            .select(
+                'personnel.personnel_id',
+                'personnel.student_id',
+                'personnel.personnel_name',
+                'personnel.email',
+                'personnel.dob',
+                'personnel.gender',
+                'personnel.address',
+                'personnel.faculty',
+                'personnel.university',
+                'personnel.major',
+                'personnel.class',
+                'personnel.cv_type',
+                'personnel.cv_link',
+                'personnel.cohort_name',
+                'personnel_status.term_id',
+                'personnel_status.department_name',
+                'personnel_status.position_name',
+                'personnel_status.personnel_status'
+            )
+            .where('personnel.personnel_id', id)
+            .first();
+
+        if (!personnel) {
+            return null;
+        }
+        return personnel;
+    },
     createPersonnelWithStatus: async (
         personnel: Personnel,
         status: {
