af2ad7cb775e9098b453d553b0849610736efabd
giahuytr <huyhaibabon861@gmail.com>
2025-04-13 17:20:31 +0700

    Feature/P2BI-470 Build API To Create A New Account (#82)

--- src/types/account.ts
+++ src/types/account.ts
@@ -1,7 +1,7 @@
 export interface Account {
     account_id?: string;
     username: string;
-    password?: string;
+    password: string;
     sysrole_id: string;
     created_on?: string;
     last_modified_on?: string;
