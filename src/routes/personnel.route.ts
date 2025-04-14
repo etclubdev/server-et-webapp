bb387683eef567962bec8fb3829990b2b38465e3
Anh Vy Tran <tr.anhvy.work@gmail.com>
2025-04-14 19:47:08 +0700

    Feature/Build API For Getting Unregistered Personnels (#90)
    * feat: build api for getting unregistered personnels
    
    * fix: optimize query and change the status of responses

--- src/routes/personnel.route.ts
+++ src/routes/personnel.route.ts
@@ -4,10 +4,12 @@ import validate from "../middlewares/validate.mdw";
 import { createPersonnelWithStatusSchema, updatePersonnelSchema } from "../entities/personnel.entity";
 
 const router = express.Router();
-router.delete("/", personnelController.deleteMultiplePersonnels);
+
+router.delete("/bulk-delete", personnelController.deleteMultiplePersonnels);
 router.delete("/:id", personnelController.deletePersonnel);
 router.put("/:id", validate(updatePersonnelSchema), personnelController.updatePersonnel);
 router.get("/", personnelController.getPersonnels);
+router.get("/unregistered", personnelController.getUnregisteredPersonnels);
 router.get("/:id", personnelController.getPersonnelByID);
 router.post("/", validate(createPersonnelWithStatusSchema), personnelController.createPersonnelWithStatus);
 
--- src/services/personnel.service.ts
+++ src/services/personnel.service.ts
@@ -2,23 +2,27 @@ import db from '../utils/db.util';
 import { Personnel } from '../types/personnel';
 
 export default {
+    getUnregisteredPersonnels: async () => {
+        const personnels = await db('personnel')
+            .join('personnel_status', 'personnel.personnel_id', 'personnel_status.personnel_id')
+            .leftJoin('account', 'personnel.personnel_id', 'account.personnel_id')
+            .whereNull('account.personnel_id')
+            .select(
+                'personnel.*',
+                'personnel_status.*'
+            );
+        return personnels;
+    },
     getPersonnelByDepartment: async (departmentName: string): Promise<any[]> => {
         if (!departmentName) {
             throw new Error("Invalid Data: departmentName is required");
         }
 
-
         const personnels = await db('personnel')
             .join('personnel_status', 'personnel.personnel_id', 'personnel_status.personnel_id')
             .select(
-                'personnel.personnel_id',
-                'personnel.student_id',
-                'personnel.personnel_name',
-                'personnel.email',
-                'personnel_status.term_id',
-                'personnel_status.department_name',
-                'personnel_status.position_name',
-                'personnel_status.personnel_status'
+                'personnel.*',
+                'personnel_status.*'
             )
             .where('personnel_status.department_name', departmentName);
 
@@ -54,7 +58,7 @@ export default {
 
         try {
 
-            const deletedAccount= await trx('account')
+            const deletedAccount = await trx('account')
                 .where('personnel_id', id)
                 .del();
 
@@ -135,24 +139,8 @@ export default {
         const personnel = await db('personnel')
             .join('personnel_status', 'personnel.personnel_id', 'personnel_status.personnel_id')
             .select(
-                'personnel.personnel_id',
-                'personnel.student_id',
-                'personnel.personnel_name',
-                'personnel.email',
-                'personnel.dob',
-                'personnel.gender',
-                'personnel.address',
-                'personnel.faculty',
-                'personnel.university',
-                'personnel.major',
-                'personnel.class',
-                'personnel.cv_type',
-                'personnel.cv_link',
-                'personnel.cohort_name',
-                'personnel_status.term_id',
-                'personnel_status.department_name',
-                'personnel_status.position_name',
-                'personnel_status.personnel_status'
+                'personnel.*',
+                'personnel_status.*'
             )
             .where('personnel.personnel_id', id)
             .first();
@@ -198,24 +186,8 @@ export default {
         const personnels = await db('personnel')
             .join('personnel_status', 'personnel.personnel_id', 'personnel_status.personnel_id')
             .select(
-                'personnel.personnel_id',
-                'personnel.student_id',
-                'personnel.personnel_name',
-                'personnel.email',
-                'personnel.dob',
-                'personnel.gender',
-                'personnel.address',
-                'personnel.faculty',
-                'personnel.university',
-                'personnel.major',
-                'personnel.class',
-                'personnel.cv_type',
-                'personnel.cv_link',
-                'personnel.cohort_name',
-                'personnel_status.term_id',
-                'personnel_status.department_name',
-                'personnel_status.position_name',
-                'personnel_status.personnel_status'
+                'personnel.*',
+                'personnel_status.*'
             );
 
         if (personnels.length === 0) {
@@ -232,24 +204,8 @@ export default {
         const personnels = await db('personnel')
             .join('personnel_status', 'personnel.personnel_id', 'personnel_status.personnel_id')
             .select(
-                'personnel.personnel_id',
-                'personnel.student_id',
-                'personnel.personnel_name',
-                'personnel.email',
-                'personnel.dob',
-                'personnel.gender',
-                'personnel.address',
-                'personnel.faculty',
-                'personnel.university',
-                'personnel.major',
-                'personnel.class',
-                'personnel.cv_type',
-                'personnel.cv_link',
-                'personnel.cohort_name',
-                'personnel_status.term_id',
-                'personnel_status.department_name',
-                'personnel_status.position_name',
-                'personnel_status.personnel_status'
+                'personnel.*',
+                'personnel_status.*'
             )
             .where('personnel_status.personnel_status', status);
 
