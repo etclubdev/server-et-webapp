76b6689989247fb9e37d15af4fe88135e2b73752
giahuytr <huyhaibabon861@gmail.com>
2025-04-15 00:23:15 +0700

    Feat/ Build API To Get All Terms (#93)

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import personnelRoute from './routes/personnel.route';
 import systemRoleRoute from './routes/systemRole.route';
 import bannerRoute from './routes/banner.route';
 import accountRoute from './routes/account.route'
+import termRoute from './routes/term.route'
 
 const app = express();
 
@@ -32,6 +33,7 @@ app.use("/personnels", personnelRoute);
 app.use('/system-role', systemRoleRoute);
 app.use('/banners', bannerRoute);
 app.use('/accounts', accountRoute)
+app.use('/terms', termRoute)
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
