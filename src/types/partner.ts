4a13ae1c112cd43de600fb8f64109c6f16b659bc
Gia Huy <huyhaibabon861@gmail.com>
2025-04-14 20:40:03 +0700

    fix: update partner entity

--- src/types/partner.ts
+++ src/types/partner.ts
@@ -1,6 +1,6 @@
 export interface Partner {
     partner_name: string
-    partner_category_id: string;
+    partner_category_name: string;
     avatar_url?: string;
     short_description?: string;
     email: string;
