eee4db34e9ce297ee768c0c6325450db879e0c31
Nguyễn Trường Duy <168575514+TrgDuyNg@users.noreply.github.com>
2025-04-14 17:04:01 +0700

    Feature/P2BI-366 Build API To Get Achievement By ID (#85)
    Co-authored-by: avytran <tr.anhvy.work@gmail.com>

--- src/controllers/achievement.controller.ts
+++ src/controllers/achievement.controller.ts
@@ -2,6 +2,35 @@ import { Request, Response } from "express";
 import achievementService from "../services/achievement.service";
 
 export default {
+    getAchievementById: async (req: Request, res: Response) => {
+        try {
+            const { id } = req.params;
+
+            const achievement = await achievementService.getAchievementById(id);
+
+            if (!achievement) {
+                res.status(404).json({
+                    message: "The achievement does not exist",
+                    data: null
+                });
+                return;
+            }
+
+            res.status(200).json({
+                message: "Successfully retrieved achievement",
+                data: achievement
+            });
+            return;
+
+        } catch (error) {
+            console.error("Error retrieving achievement:", error);
+            res.status(500).json({
+                message: "Internal Server Error: " + error.message
+            });
+            return;
+        }
+    },
+
     createAchievement: async (req: Request, res: Response): Promise<void> => {
         const achievement = req.body;
         try {
--- src/routes/achievement.route.ts
+++ src/routes/achievement.route.ts
@@ -6,6 +6,7 @@ import { createAchievementSchema } from "../entities/achievement.entity";
 
 const router = express.Router();
 
+router.get("/:id", achievementController.getAchievementById);
 router.post("/", validate(createAchievementSchema), achievementController.createAchievement);
 router.get("/", achievementController.getAllAchievements);
 
