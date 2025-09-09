249b9fa3ce908ce8097b5b476f4779908cc4fe68
Nguyễn Trường Duy <168575514+TrgDuyNg@users.noreply.github.com>
2025-09-10 03:07:57 +0700

    Feature/ Build api to export applications (#141)
    * feature: build api to export applications
    
    * feat: add dependencies
    
    * refactor: enhance controller and route
    
    ---------
    
    Co-authored-by: avytran <tr.anhvy.work@gmail.com>

--- src/routes/application.route.ts
+++ src/routes/application.route.ts
@@ -7,6 +7,7 @@ import { managePersonnelRole } from '../global/roles';
 const router = express.Router();
 
 router.get('/', authGuard.verifyRoles(managePersonnelRole), applicationController.getApplications);
+router.get('/export', authGuard.verifyRoles(managePersonnelRole), applicationController.exportApplications);
 router.get('/statistics', authGuard.verifyRoles(managePersonnelRole), applicationController.statisticsApplication);
 router.get('/:id', authGuard.verifyRoles(managePersonnelRole), authGuard.verifyDepartmentForManageApplication(), applicationController.getApplicationById);
 router.put('/restore', authGuard.verifyRoles(managePersonnelRole), authGuard.verifyDepartmentForManageApplication(), applicationController.restoreApplication);
