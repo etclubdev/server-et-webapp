ec9f8ed6f46b732352159899f9293812035f7104
avytran <tr.anhvy.work@gmail.com>
2025-09-14 21:42:39 +0700

    fix: recruitment service

--- src/controllers/recruitment.controller.ts
+++ src/controllers/recruitment.controller.ts
@@ -5,8 +5,8 @@ import recruitmentService from "../services/recruitment.service";
 export default {
     getStatusofFirstRecruitment: async (req: Request, res: Response): Promise<void> => {
         try {
-            const status = await recruitmentService.getStatusofFirstRecruitment();
-            if (status === null) {
+            const recruitment = await recruitmentService.getStatusofFirstRecruitment();
+            if (recruitment === null) {
                 res.status(404).json({
                     message: "No recruitment found",
                     data: null
@@ -15,7 +15,7 @@ export default {
             }
             res.status(200).json({
                 message: "Retrieved status of first recruitment successfully",
-                data: status
+                data: recruitment
             });
             return;
         } catch (error) {
